ca9d57c8a7247d1e4aa6acca28edc17f25c175c6
Tiago Barbosa <tiago@farias.cc>
2022-09-01 17:52:39 -0300

    Java Collections: Trabalhando com ArrayList

---
+++ java-collections/src/br/com/tiagobarbosa/java/collections/main/Main.java
@@ -0,0 +1,32 @@
+package br.com.tiagobarbosa.java.collections.main;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) {
+        String class01 = "Trabalhando com ArrayList";
+        String class02 = "Lista de objetos";
+        String class03 = "Relacionamento com coleções";
+
+        ArrayList<String> classes = new ArrayList<>();
+        classes.add(class01);
+        classes.add(class02);
+        classes.add(class03);
+
+        System.out.println(classes);
+
+//        classes.remove(0);
+//        System.out.println(classes);
+
+        for (String clazz :
+                classes) {
+            System.out.println("Class: " + clazz);
+        }
+
+        Collections.sort(classes);
+        System.out.println(classes);
+        System.out.println("For each class:");
+        classes.forEach(clazz -> System.out.printf(" - %s%n", clazz));
+    }
+}
